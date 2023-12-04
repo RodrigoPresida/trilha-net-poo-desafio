d91e45ee06c1d85633721f3dbdb3282ccad5d215
Rodrigo Presida <rodrigopresida@gmail.com>
2023-12-04 20:00:20 -0300

    Projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone");
+Smartphone iphoneplus = new Iphone(numero: "2345678", modelo: "iphoneplus", imei: "0102030405", memoria: 128);
+iphoneplus.Ligar();
+iphoneplus.ReceberLigacao();
+iphoneplus.InstalarAplicativo("Instagram");
+
+Console.WriteLine("Nokia");
+Smartphone lumia = new Nokia(numero: "2345678", modelo: "Nokia Lumia", imei: "0102030405", memoria: 128);
+lumia.Ligar();
+lumia.ReceberLigacao();
+lumia.InstalarAplicativo("WhatsApp");
